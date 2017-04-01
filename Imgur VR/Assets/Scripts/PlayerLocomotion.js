bfbc7814a9d9a688d05e7c2bdb8f174773d925c1
Ben Powell <benjamindavidpowell@gmail.com>
2017-04-01 14:50:03 -0400

    Added more stuff

--- Imgur VR/Assets/Scripts/PlayerLocomotion.js
+++ Imgur VR/Assets/Scripts/PlayerLocomotion.js
@@ -5,33 +5,12 @@ import System.Collections.Generic;
  * After we get VR running, this will probably be replaced.
  */
 public var movementSpeed : float = 2.0F;
-public var turnSpeed: float = 15.0F;
-
-// Mouselook code from answers.unity3d.com/questions/29741/mouse-look-script.html
-private var initialRotation : Quaternion;
-var rotationX : float;
-var rotationY : float;
 
 function Start () {
-	initialRotation = transform.localRotation;
+
 }
 
 function Update () {
 	var speed: float = movementSpeed * Time.deltaTime;
 	transform.Translate(Input.GetAxis("Horizontal") * speed, 0.0F, Input.GetAxis("Vertical") * speed);
-	
-	rotationX += ClampAngle(Input.GetAxis("Mouse X") * turnSpeed, -360, 360);
-	rotationY += ClampAngle(Input.GetAxis("Mouse Y") * turnSpeed, -180, 180);
-	var qx : Quaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-	var qy : Quaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
-	
-	transform.localRotation = initialRotation * qx * qy;
-}
-
-function ClampAngle (angle : float, min : float, max: float) {
-     if (angle < -360F)
-         angle += 360F;
-     if (angle > 360F)
-         angle -= 360F;
-     return Mathf.Clamp (angle, min, max);
 }
