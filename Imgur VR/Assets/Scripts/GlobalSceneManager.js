26d6cb2013d7e20f7c06254bd68897709d32c7ca
Ben Powell <benjamindavidpowell@gmail.com>
2017-04-02 03:58:24 -0400

    added plugins

--- Imgur VR/Assets/Scripts/GlobalSceneManager.js
+++ Imgur VR/Assets/Scripts/GlobalSceneManager.js
@@ -11,23 +11,23 @@ public static class GlobalSceneManager  {
 	public var input3: String;
 	public var song: String;
 
-	public function setsong(s0ng){
+	public function setsong(s0ng: String){
 		song = s0ng;
 		Debug.Log(song);
 	}
 
 	//Set inputstrings
-	public function setMeme1(s1){
+	public function setMeme1(s1: String){
 		input1 =s1;
 		Debug.Log(input1);
 
 	}
-	public function setMeme2(s1){
+	public function setMeme2(s1: String){
 		input2 =s1;
 		Debug.Log(input2);
 
 	}
-	public function setMeme3(s1){
+	public function setMeme3(s1: String){
 		input3 =s1;
 		Debug.Log(input3);
 
