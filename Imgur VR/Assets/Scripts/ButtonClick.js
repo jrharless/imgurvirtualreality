d58d229c3129b6e0086675a3d4787734a3ce6938
Jacob Harless <jrharless@email.wm.edu>
2017-04-02 04:04:47 -0400

    added menu

---
+++ Imgur VR/Assets/Scripts/ButtonClick.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+//import UnityEngine.UI.Button;
+//import GlobalSceneManager;
+//public class ClickExample extends MonoBehaviour {
+//	public var yourButton: UnityEngine.UI.Button;
+//	function Start() {
+//		var btn: UnityEngine.UI.Button = yourButton.GetComponent.<UnityEngine.UI.Button>();
+//		btn.onClick.AddListener(TaskOnClick);
+//	}
+//	function TaskOnClick() {
+//		Debug.Log("You have clicked the button!");
+//		
+//	}
+//}
+
+function TaskOnClick() {
+	GlobalSceneManager.goToMemeSea();
+	Debug.Log("You have clicked the button!");
+
+}
---
+++ Imgur VR/Assets/Scripts/InputField.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+import UI.InputField;
+import UI.Text;	
+public function onDeselect(data:String) {
+	var kids: UnityEngine.UI.Text[] = GetComponentsInChildren.<UnityEngine.UI.Text>() as UnityEngine.UI.Text[];
+	//var kids: InputField[] = GetComponentsInChildren(InputField) as InputField[];
+	var s1 : String = "";
+	var s2 : String = "";
+	var s3 : String = "";
+	switch (data) {
+	case "meme1":
+		Debug.Log(kids[3].text);
+
+		if (kids[3].text != "Enter Search"){
+			s1 = kids[3].text;
+			GlobalSceneManager.setMeme1(s1);
+		}
+
+		break;
+	case "meme2":
+		Debug.Log(kids[5].text);
+
+		if (kids[5].text != "Enter Search"){
+			s2 = kids[5].text;
+			GlobalSceneManager.setMeme2(s2);
+		}
+
+		break;
+	case "meme3":
+		Debug.Log(kids[7].text);
+
+		if (kids[7].text != "Enter Search"){
+			s3 = kids[7].text;
+			GlobalSceneManager.setMeme3(s3);
+		}
+
+		break;
+	}
+
+
+
+		
+}
